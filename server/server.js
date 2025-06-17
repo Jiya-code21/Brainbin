0e97ec7f47b19d04d93d0e8d5a9ec26770e15c41
Jiya Agrawal <jiyaagrawal6395@gmail.com>
2025-06-17 22:49:40 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,32 +1,27 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import 'dotenv/config';
-import cookieParser from "cookie-parser"
-import connectDB from "./config/mongodb.js"
-import authRouter from "./routes/authroutes.js"
-import userRouter from "./routes/userroutes.js"
+import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authroutes.js";
+import userRouter from "./routes/userroutes.js";
+import noteRouter from "./routes/noteroutes.js";
 
+const app = express();
+const port = process.env.PORT || 4000;
 
-const app=express()
-const port=process.env.PORT || 4000
-connectDB()
+connectDB();
 
-const allowedOrigins=['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173'];
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
-    origin:allowedOrigins,
-    credentials:true
-}))
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
 
-app.get('/',(req,res)=>
-res.send("API Working")
-)
-app.use('/api/auth',authRouter) 
-app.use('/api/user',userRouter) 
+app.get("/", (req, res) => res.send("API Working"));
 
-app.listen(port,()=>
-    console.log(`Server started on PORT:${port}`)
-)
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/notes", noteRouter);
 
+app.listen(port, () => console.log(`Server running on port ${port}`));
