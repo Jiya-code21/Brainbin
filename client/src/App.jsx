6a37619ab10f61e15e48083cbdf045847c0fcdfa
Jiya Agrawal <jiyaagrawal6395@gmail.com>
2025-06-18 22:23:24 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom'
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import EmailVerify from "./pages/EmailVerify"
-import ResetPassword from "./pages/ResetPassword"
-  import { ToastContainer, toast } from 'react-toastify';
-import Notes from "./pages/Notes"
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import EmailVerify from "./pages/EmailVerify";
+import ResetPassword from "./pages/ResetPassword";
+import Notes from "./pages/Notes";
+import Error from "./pages/error"; 
+import { ToastContainer } from 'react-toastify';
+
 function App() {
   return (
     <div>
-      <ToastContainer/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/email-verify' element={<EmailVerify/>}/>
-      <Route path='/reset-password' element={<ResetPassword/>}/>
-      <Route path="/notes" element={<Notes />} />
+      <ToastContainer />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/email-verify' element={<EmailVerify />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path="/notes" element={<Notes />} />
 
-    </Routes>
+        {/* ✅ Add 404 route at the end */}
+        <Route path='*' element={<Error />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
