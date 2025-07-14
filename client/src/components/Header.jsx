c8e6f34cd8478d8b9599c0667f774247c68f4598
Jiya Agrawal <jiyaagrawal6395@gmail.com>
2025-07-14 12:16:49 +0530

    Update CTA button gradient color

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
-import { assets } from "../assets/assets";
+import { assets } from "../assets/assets"; 
 import { AppContent } from "../context/AppContext";
 
 const { header_img, hand_wave, demoVideo } = assets;
