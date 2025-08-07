be9bcc1da9cd7103bb17b7910a18d680834d6f81
Jiya Agrawal <jiyaagrawal6395@gmail.com>
2025-08-08 01:53:54 +0530

    Add toggle star add route .

--- server/routes/noteroutes.js
+++ server/routes/noteroutes.js
@@ -3,8 +3,10 @@ import {
   createNote,
   getUserNotes,
   deleteNote,
-  updateNote
+  updateNote,
+  toggleStarNote,
 } from "../controllers/notecontroller.js";
+
 import userAuth from "../middleware/useAuth.js";
 
 const noteRouter = express.Router();
@@ -13,5 +15,6 @@ noteRouter.post("/create", userAuth, createNote);
 noteRouter.get("/my-notes", userAuth, getUserNotes);
 noteRouter.put("/update/:id", userAuth, updateNote);
 noteRouter.delete("/delete/:id", userAuth, deleteNote);
+noteRouter.patch("/star/:id", userAuth, toggleStarNote);
 
 export default noteRouter;
