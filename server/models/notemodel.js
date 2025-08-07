66004c3a6590e5b8cef5a6f635824de59861ddf0
Jiya Agrawal <jiyaagrawal6395@gmail.com>
2025-08-08 01:52:02 +0530

    Add model schema for the starred feature in cards.

--- server/models/notemodel.js
+++ server/models/notemodel.js
@@ -1,38 +1,44 @@
 import mongoose from "mongoose";
 
-const noteSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-    required: true,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  subject: {
-    type: String,
-    default: "",
-  },
-  tags: [
-    {
-      type: String,
+const noteSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
     },
-  ],
-  resourceUrl: {
-    type: String,
-    default: "",
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    subject: {
+      type: String,
+      default: "",
+    },
+    tags: [
+      {
+        type: String,
+      },
+    ],
+    resourceUrl: {
+      type: String,
+      default: "",
+    },
+    status: {
+      type: String,
+      default: "Concepts",
+    },
+    isStarred: {
+      type: Boolean,
+      default: false,
+    },
   },
-  status: {
-    type: String,
-    default: "Concepts", // default status if not provided
-  },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const noteModel = mongoose.models.note || mongoose.model("note", noteSchema);
-
 export default noteModel;
